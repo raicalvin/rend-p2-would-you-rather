7b578f23bc3785fad3ec95b7005cf2d85e1e3a72
Calvin Rai <raicalvin@gmail.com>
2018-11-07 13:41:30 -0800

    Created NavBar initial component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import NavBar from "./components/NavBar.js";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <div className="App">
+        <h2>Would You Rather?</h2>
+        <NavBar />
+      </div>
+    );
   }
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Leaderboard</li>
+        <li>Add Poll</li>
+        <li>Calvin S.</li>
+        <li>Logout</li>
+      </ul>
+    </nav>
+  );
+}
