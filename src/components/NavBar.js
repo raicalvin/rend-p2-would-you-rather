af41f37ca7f4892988cdeed4e1bdbcd2bddd0a54
Calvin Rai <raicalvin@gmail.com>
2018-11-08 14:34:46 -0800

    Added initial Links and Router to NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav class="navbar navbar-inverse">
-      <div class="container-fluid">
-        <ul class="nav navbar-nav">
-          <li class="active">
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Leaderboard</a>
-          </li>
-          <li>
-            <a href="#">New Poll</a>
-          </li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li>
-            <a href="#">
-              <span class="glyphicon glyphicon-user" /> Calvin S.
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <span class="glyphicon glyphicon-log-in" /> Logout
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Router>
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <ul class="nav navbar-nav">
+            <li class="active">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/leaderboard">Leaderboard</Link>
+            </li>
+            <li>
+              <Link to="/add">New Poll</Link>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <a href="#">
+                <span class="glyphicon glyphicon-user" /> Calvin S.
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="glyphicon glyphicon-log-in" /> Logout
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </Router>
   );
 }
