8a926174efaf70d7b1fcec26b5da4f267a0a9b4b
Calvin Rai <raicalvin@gmail.com>
2018-11-07 18:40:57 -0800

    Used combineReducers to combine initial reducers for users, questions, and authedUser

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,12 @@
 // This will export our call to combineReducers
+import { combineReducers } from "redux";
+
+import authedUser from "./authedUser";
+import users from "./users";
+import questions from "./questions";
+
+export default combineReducers({
+  authedUser,
+  users,
+  questions
+});
