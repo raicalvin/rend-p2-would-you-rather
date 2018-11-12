15766d314d93fa106d1f79f07c918bbdb193a9f9
Calvin Rai <raicalvin@gmail.com>
2018-11-11 19:41:31 -0800

    Implemented className changes for active unanswered and answered buttons in Dashboard.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,8 +3,27 @@ import { connect } from "react-redux";
 import Question from "./Question.js";
 
 class Dashboard extends Component {
+  state = {
+    unansweredButtonIsActive: true
+  };
+
+  filterQuestions(questions, authedUser) {}
+
+  handleActiveButtonClick(event) {
+    console.log(`${event.target.id} was clicked!`);
+    if (event.target.id === "unanswered-poll-button") {
+      this.setState({
+        unansweredButtonIsActive: true
+      });
+    }
+    if (event.target.id === "answered-poll-button") {
+      this.setState({
+        unansweredButtonIsActive: false
+      });
+    }
+  }
+
   render() {
-    console.log(this.props.questionIds);
     /*
       Check to see who authedUser is.
       Check to see which button is active (answered or unanswered).
@@ -14,9 +33,28 @@ class Dashboard extends Component {
     return (
       <div>
         <h2>Dashboard</h2>
-        <div className="center-flex-items">
-          <button className="btn btn-primary">Unanswered Polls</button>
-          <button className="btn btn-primary active">Answered Polls</button>
+        <div
+          className="center-flex-items"
+          onClick={this.handleActiveButtonClick.bind(this)}
+        >
+          <button
+            className={
+              "btn btn-primary " +
+              (this.state.unansweredButtonIsActive ? "active" : "")
+            }
+            id="unanswered-poll-button"
+          >
+            Unanswered Polls
+          </button>
+          <button
+            className={
+              "btn btn-primary " +
+              (this.state.unansweredButtonIsActive ? "" : "active")
+            }
+            id="answered-poll-button"
+          >
+            Answered Polls
+          </button>
         </div>
 
         <ul className="center center-flex-items">
@@ -33,11 +71,12 @@ class Dashboard extends Component {
 }
 
 // Get necessary information from Redux store state
-function mapStateToProps({ questions }) {
+function mapStateToProps({ questions, authedUser }) {
   return {
     questionIds: Object.keys(questions).sort(
       (a, b) => questions[b].timestamp - questions[a].timestamp
-    )
+    ),
+    authedUser: authedUser
   };
 }
 
