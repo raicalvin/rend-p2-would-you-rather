4e829a88e61d852ee03042ecf55f32e580300931
Calvin Rai <raicalvin@gmail.com>
2018-11-26 13:43:36 -0800

    Deleted test prop for Question component

--- src/components/QuestionView.js
+++ src/components/QuestionView.js
@@ -3,22 +3,20 @@ import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import Question from "./Question";
 
-// This page will view a single question and then showcase the votes and all that information
+// This page will view a single question and then showcase the votes and scores for the answers
 
 class QuestionView extends Component {
   render() {
-    console.log("Hello, QuestionView is being called");
     const { id } = this.props.match.params;
     const { questions } = this.props.state;
     const question = questions[id];
-    console.log(question);
     if (question === undefined) {
       return <Redirect to="/not-found" />;
     }
 
     return (
       <div className="question-border center center-flex-items single-question-view">
-        <Question id={id} test={"YoOoOoOooo"} />
+        <Question id={id} />
       </div>
     );
   }
